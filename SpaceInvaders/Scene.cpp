4b82b6e5f6bd7aab2cf6de40862ec14f8b33d093
carlitzer <cjgribel@gmail.com>
2024-06-04 03:47:33 +0200

    wip

--- SpaceInvaders/Scene.cpp
+++ SpaceInvaders/Scene.cpp
@@ -1,29 +1,152 @@
 
+#include <thread>
+#include <chrono>
 #include "imgui.h"
 #include "mat.h"
 #include "Scene.hpp"
 
-// entt-sol2
-#define SOL_ALL_SAFETIES_ON 1
-#include <sol/sol.hpp>
-
 #include "bond.hpp"
 #include "transform.hpp"
+#include "kbhit.hpp"
 
 #define AUTO_ARG(x) decltype(x), x
 using namespace linalg;
 
-inline void my_panic(sol::optional<std::string> maybe_msg) {
-    std::cerr << "Lua is in a panic state and will now abort() the application" << std::endl;
-    if (maybe_msg) {
-        const std::string& msg = maybe_msg.value();
-        std::cerr << "\terror message: " << msg << std::endl;
+namespace {
+    // inline void my_panic(sol::optional<std::string> maybe_msg) {
+    //     std::cerr << "Lua is in a panic state and will now abort() the application" << std::endl;
+    //     if (maybe_msg) {
+    //         const std::string& msg = maybe_msg.value();
+    //         std::cerr << "\terror message: " << msg << std::endl;
+    //     }
+    //     // When this function exits, Lua will exhibit default behavior and abort()
+    // }
+
+    // void inspect_script(const ScriptComponent& script)
+    // {
+    //     script.self.for_each([](const sol::object& key, const sol::object& value)
+    //         { std::cout << key.as<std::string>() << ": "
+    //         << sol::type_name(value.lua_state(), value.get_type())
+    //         << std::endl; });
+    // }
+
+    void init_script(entt::registry& registry, entt::entity entity)
+    {
+        auto& script_comp = registry.get<ScriptComponent>(entity);
+        for (auto& script : script_comp.scripts)
+        {
+            assert(script.self.valid());
+            script.update = script.self["update"];
+            assert(script.update.valid());
+
+            // -> entityID?
+            script.self["id"] = sol::readonly_property([entity]
+                { return entity; });
+            // -> registry?
+            script.self["owner"] = std::ref(registry);
+            if (auto&& f = script.self["init"]; f.valid())
+                f(script.self);
+            // inspect_script(script);
+        }
     }
-    // When this function exits, Lua will exhibit default behavior and abort()
+
+    void release_script(entt::registry& registry, entt::entity entity)
+    {
+        auto& script_comp = registry.get<ScriptComponent>(entity);
+        for (auto& script : script_comp.scripts)
+        {
+            // auto& script = registry.get<ScriptComponent>(entity);
+            if (auto&& f = script.self["destroy"]; f.valid())
+                f(script.self);
+            script.self.abandon();
+        }
+    }
+
+    void script_system_update(entt::registry& registry, float delta_time)
+    {
+        auto view = registry.view<ScriptComponent>();
+        for (auto entity : view)
+        {
+            auto& script_comp = registry.get<ScriptComponent>(entity);
+            for (auto& script : script_comp.scripts)
+            {
+                // auto& script = view.get<ScriptComponent>(entity);
+                assert(script.self.valid());
+                script.update(script.self, delta_time);
+            }
+        }
+    }
 }
 
 bool Scene::init()
 {
+#if 1
+
+    try
+    {
+        register_meta_component<Transform>();
+
+        // entt::registry registry{};
+        registry.on_construct<ScriptComponent>().connect<&init_script>();
+        registry.on_destroy<ScriptComponent>().connect<&release_script>();
+
+        // sol::state lua{};
+        lua.open_libraries(sol::lib::base, sol::lib::package, sol::lib::string);
+        lua.require("registry", sol::c_call<AUTO_ARG(&open_registry)>, false);
+        register_transform(lua); // Make Transform struct available to Lua
+
+        sol::load_result behavior_script = lua.load_file("lua/behavior.lua");
+        sol::protected_function script_function = behavior_script;
+
+        assert(behavior_script.valid());
+
+        for (int i = 0; i < 5; ++i)
+        {
+            auto e = registry.create();
+            registry.emplace<Transform>(e, Transform{ i, i });
+
+            sol::table script_table = script_function();
+
+            ScriptComponent script_comp;
+            ScriptComponent::Script script{ script_table };
+            script_comp.scripts.push_back(script);
+            registry.emplace<ScriptComponent>(e, script_comp);
+        }
+
+        using namespace std::chrono_literals;
+
+        // constexpr auto target_frame_time = 500ms;
+        int delta_time_ms{ 500 };
+
+        while (true)
+        {
+            using clock = std::chrono::high_resolution_clock;
+            const auto begin_ticks = clock::now();
+
+            script_system_update(registry, delta_time_ms * 0.001f);
+            // std::this_thread::sleep_for(target_frame_time);
+            std::this_thread::sleep_for(std::chrono::milliseconds(delta_time_ms));
+
+            //delta_time = std::chrono::duration_cast<fsec>(clock::now() - begin_ticks);
+            //if (delta_time > 1s)
+            //    delta_time = target_frame_time;
+
+            if (_kbhit())
+                break;
+        }
+        registry.clear();
+    }
+    // catch (const std::exception& e)
+    catch (const sol::error& e)
+    {
+        std::cout << "exception: " << e.what();
+        return -1;
+    }
+#else
+    // Registry test
+    // lua/registry_simple.lua, lua/iterate_entities.lua
+    // Linking an entt::registry to Lua, creating entities, 
+    // adding & modifying components, iterating & removing components
     try {
         register_meta_component<Transform>();
 
@@ -50,12 +173,14 @@ bool Scene::init()
         //     assert(false)
         // )");
 
-        std::cout << "All Transforms:" << std::endl;
-        auto view = registry.view<Transform>();
-        for (auto& ent : view)
         {
-            auto& t = view.get<Transform>(ent);
-            std::cout << t.to_string() << std::endl;
+            std::cout << "All Transforms:" << std::endl;
+            auto view = registry.view<Transform>();
+            for (auto& ent : view)
+            {
+                auto& t = view.get<Transform>(ent);
+                std::cout << t.to_string() << std::endl;
+            }
         }
 
         const auto bowser = lua["bowser"].get<entt::entity>();
@@ -64,35 +189,44 @@ bool Scene::init()
         const Transform& transform = lua["transform"];
         assert(xf->x == transform.x && xf->y == transform.y);
 
-
-        entt::runtime_view view2{};
-        view2.iterate(registry.storage<Transform>());
-        std::cout << "Initial size hint: " << view2.size_hint() << std::endl;
-        //
-        entt::runtime_view view3{};
-        view3.iterate(registry.storage<Transform>());
-        for (auto ent : view3)
-            registry.remove<Transform>(ent);
-        //
-        entt::runtime_view view4{};
-        view4.iterate(registry.storage<Transform>());
-        // auto view2 = registry.runtime_view<Transform>();
-        std::cout << "Size hint after removal: " << view4.size_hint() << std::endl;
-
-
-        /*
-                //
-                // lua.do_file("lua/iterate_entities.lua");
-                lua.safe_script_file("lua/iterate_entities.lua");
-                assert(registry.orphan(bowser) && "The only component (Transform) should  "
-                    "be removed by the script");
-        */
-        std::cout << "All Transforms:" << std::endl;
-        auto view2_ = registry.view<Transform>();
-        for (auto& ent : view2_)
+#if 0
         {
-            auto& t = view2_.get<Transform>(ent);
-            std::cout << t.to_string() << std::endl;
+            // print 'size_hint'
+            entt::runtime_view view{};
+            view.iterate(registry.storage<Transform>());
+            std::cout << "Initial size hint: " << view.size_hint() << std::endl;
+        }
+        {
+            // Remove Component for all entities
+            entt::runtime_view view3{};
+            view3.iterate(registry.storage<Transform>());
+            for (auto ent : view3)
+                registry.remove<Transform>(ent);
+            std::cout << "Removed Transform from all entities" << std::endl;
+        }
+        {
+            // print 'size_hint' again
+            entt::runtime_view view4{};
+            view4.iterate(registry.storage<Transform>());
+            // auto view2 = registry.runtime_view<Transform>();
+            std::cout << "Size hint after removal: " << view4.size_hint() << std::endl;
+        }
+#endif
+
+        //
+        // lua.do_file("lua/iterate_entities.lua");
+        lua.safe_script_file("lua/iterate_entities.lua");
+        assert(registry.orphan(bowser) && "The only component (Transform) should  "
+            "be removed by the script");
+
+        {
+            std::cout << "All Transforms:" << std::endl;
+            auto view = registry.view<Transform>();
+            for (auto& ent : view)
+            {
+                auto& t = view.get<Transform>(ent);
+                std::cout << t.to_string() << std::endl;
+            }
         }
     }
     catch (const sol::error& e)
@@ -100,15 +234,16 @@ bool Scene::init()
         std::cerr << "Lua script execution failed: " << e.what() << std::endl;
         return false;
     }
+#endif
 
     // Do some entt stuff
-    entt::registry registry;
-    auto ent1 = registry.create();
-    struct Tfm
-    {
-        float x, y, z;
-    };
-    registry.emplace<Tfm>(ent1, Tfm{});
+    // entt::registry registry;
+    // auto ent1 = registry.create();
+    // struct Tfm
+    // {
+    //     float x, y, z;
+    // };
+    // registry.emplace<Tfm>(ent1, Tfm{});
 
     return true;
 }
--- SpaceInvaders/Scene.hpp
+++ SpaceInvaders/Scene.hpp
@@ -2,14 +2,36 @@
 #define Scene_hpp
 #pragma once
 
+// entt-sol2
+#define SOL_ALL_SAFETIES_ON 1
+#include <sol/sol.hpp>
+
 #include <entt/entt.hpp> // -> Scene source
+#include <vector>
+
 #include "vec.h"
 #include "SceneBase.h"
+
+// ScriptedBehaviorComponent
+struct ScriptComponent
+{
+    // BehaviorScript
+    struct Script
+    {
+        sol::table self;
+        // sol::function init;
+        // sol::function destroy;
+        sol::function update;
+    };
+    std::vector<Script> scripts;
+};
 
 class Scene : public eeng::SceneBase
 {
 protected:
-    entt::registry registry;
+    sol::state lua{}; //(sol::c_call<decltype(&my_panic), &my_panic>);
+    entt::registry registry{};
+
     linalg::v3f lightPos, eyePos;
     const float nearPlane = 1.0f, farPlane = 500.0f;
     int drawcallCount = 0;
