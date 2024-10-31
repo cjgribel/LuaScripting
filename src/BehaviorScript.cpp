cb0c5871b08aa1d2045538d33a6ccdeddf6a27bb
carlitzer <cjgribel@gmail.com>
2024-11-01 00:59:09 +0100

    BehaviorScript to its own source files

---
+++ src/BehaviorScript.cpp
@@ -0,0 +1,9 @@
+
+//#include <iostream>
+#include <entt/entt.hpp>
+
+#define SOL_ALL_SAFETIES_ON 1
+#include <sol/sol.hpp>
+
+#include "BehaviorScript.hpp"
+
---
+++ src/BehaviorScript.hpp
@@ -0,0 +1,15 @@
+#ifndef BehaviorScript_hpp
+#define BehaviorScript_hpp
+
+#include <cassert>
+#include <vector>
+
+// sol is used by
+//      For ScriptedBehaviorComponent => its own hpp/cpp
+//      Lua event
+#define SOL_ALL_SAFETIES_ON 1
+#include <sol/sol.hpp> 
+//#include <sol/forward.hpp>
+
+
+#endif // CoreComponents_hpp
